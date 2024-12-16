b75503ac60d8d332adf2fb7c9946c81d93f9b523
Justin Aquilante <JWaquilante@gmail.com>
2024-12-15 20:52:33 -0500

    day 6

--- src/main/day6/Day6Solver.java
+++ src/main/day6/Day6Solver.java
@@ -2,15 +2,20 @@ package main.day6;
 
 public class Day6Solver {
 
-    public static final String FILEPATH = "src/main/resources/inputs/day6-test.txt";
+    public static final String FILEPATH = "src/main/resources/inputs/day6-real.txt";
 
     public static void main(String[] args) {
-            Day6Parser parser = new Day6Parser(FILEPATH);
-            System.out.println("The guard is at " + parser.currentGuardPosX + " , " + parser.currentGuardPosY);
-            Day6Part1Solver part1 = new Day6Part1Solver(parser);
-            int answer1 = part1.solve();
-            System.out.println("Part 1 Answer: " + answer1);
+        Day6Parser parser = new Day6Parser(FILEPATH);
+        System.out.println("The guard is at " + parser.currentGuardPosX + " , " + parser.currentGuardPosY);
+        Day6Part1Solver part1 = new Day6Part1Solver(parser);
+        int answer1 = part1.solve();
+        System.out.println("Part 1 Answer: " + answer1);
 
-        }
+        Day6Parser parser2 = new Day6Parser(FILEPATH);
+        Day6Part2Solver part2 = new Day6Part2Solver(parser2);
+        int answer2 = part2.solve();
+        System.out.println("Part 2 Answer: " + answer2);
+
+    }
 
 }
